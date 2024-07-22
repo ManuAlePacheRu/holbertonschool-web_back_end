14832d00b2630727ea7e2e9c1d71bd6994a5dc0e
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-22 14:12:11 -0300

    Identation Fixed Task 01

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      console.log('Hello Friends!');
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    console.log('Hello Friends!');
   }
+
+  return [task, task2];
+}
