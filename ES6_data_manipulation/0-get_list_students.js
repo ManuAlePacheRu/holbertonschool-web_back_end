9c6095625d0397b4bd530b3cf98fb0fc48947875
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-24 10:24:08 -0300

    ES6 Data Manipulation Task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,8 @@
-function getListStudents(arr) {
-
+export default function getListStudents() {
+  const listOfStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return listOfStudents;
 }
