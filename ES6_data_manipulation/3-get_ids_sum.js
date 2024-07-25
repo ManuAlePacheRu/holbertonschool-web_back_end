cd0cb67f98e310d9fed683b12843302eb6076d63
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-25 14:19:27 -0300

    ES6 Data Manipulation Task 03

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(studList) {
-  const sumOfId = studList.reduce((acc, studPos) => acc + studPos.id,0,);
+  const sumOfId = studList.reduce((acc, studPos) => acc + studPos.id, 0);
   return sumOfId;
 }
