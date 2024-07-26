f0d931aa19d0e1f66f598243de0959169850db4b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-07-26 11:37:59 -0300

    Task 05 Fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -7,7 +7,6 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-
   dataView.setInt8(position, value);
 
   return dataView;
