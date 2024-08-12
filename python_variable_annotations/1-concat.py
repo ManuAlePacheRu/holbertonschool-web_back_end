5f1ad1fe2a8de1ba6682060a0120235b56c51faa
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-12 13:37:25 -0300

    Variable Annotations Task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This function concatenate two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+     Concatenate two strings.
+
+    Parameters:
+    str1 (str): The first string to concatenate.
+    str2 (str): The second string to concatenate.
+
+    Returns:
+    str: The concatenation of the two strings.
+    """
+    return str1 + str2
