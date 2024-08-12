24e9237128fd0a239a94139e299a1e4713cafe7c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-12 13:23:41 -0300

    Variable Annotation Task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def sum(a: float, b: float) -> float:
+    """
+    Calculate the sum of two floating-point numbers.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
