f1091c22be394727cd14c2f83fd77e53a08fd4f0
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-12 14:05:00 -0300

    Variable Annotations Task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+    This function returns the string representation if a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to a string.
+
+    Parameters:
+    n (float): The number to convert.
+
+    Returns:
+    str: The string representation of
+    """
+    return str(n)
