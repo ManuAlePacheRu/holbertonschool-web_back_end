4880a2d98b04827189015340984f6b580809364b
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-12 14:37:23 -0300

    Task 05

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+    Sum the values of imput list
+'''
+
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """
+     Convert a floating-point number to a string.
+
+    Parameters:
+    n (float): The number to convert.
+
+    Returns:
+    str: The string representation of the number.
+    """
+
+    return sum(input_list)
