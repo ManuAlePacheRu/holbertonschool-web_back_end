951ed3839c832b938aa366fb05b17b62c811df35
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-12 14:48:24 -0300

    Pycodestyle Fix Task 06

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,15 +2,17 @@
 import typing
 
 """
-    Sum the values of a list of int and float 
+    Sum the values of a list of int and float
 """
+
 
 def sum_mixed_list(mxd_list: typing.List[typing.Union[int, float]]) -> float:
     """
     Calculate the sum of a list containing integers and floating-point numbers.
 
     Parameters:
-    mxd_list (typing.List[typing.Union[int, float]]): A list of integers and/or floating-point numbers.
+    mxd_list (typing.List[typing.Union[int, float]]):
+    A list of integers and/or floating-point numbers.
 
     Returns:
     float: The sum of the numbers in the list, represented as a float.
