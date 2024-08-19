1ab1e2aa18535ee9b30ad92048cbb90aa8ced09c
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-19 14:05:15 -0300

    Task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-
+    async comprehension
 """
 
 import asyncio
@@ -10,6 +10,6 @@ import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """ async comprehension """
     return [i async for i in async_generator()]
