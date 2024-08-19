909343ded94c15b6f41afac109c08568baa0189d
ManuAlePacheRu <manualepacheru@gmail.com>
2024-08-19 14:56:31 -0300

    Task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+    measure runtime
+"""
+
+import asyncio
+import typing
+import time
+
+async_comp = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ measure runtime """
+    s_time = time.time()
+    tasks = []
+    for i in range(4):
+        asyncio.gather(async_comp())
+    await asyncio.gather(*tasks)
+
+    e_time = time.time()
+
+    return s_time - e_time
