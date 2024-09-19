15a83939691763e5c336a520db9afdfc304df3c8
ManuAlePacheRu <manualepacheru@gmail.com>
2024-09-19 09:50:38 -0300

    Task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,5 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (name) => process.stdout.write(`Your name is: ${name}`));
+
+process.stdin.on('end', () => console.log('This important software is now closing'));
